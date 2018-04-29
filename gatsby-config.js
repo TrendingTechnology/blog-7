45a4a55915daad6f1541f57032ca7a5659facd8a
bondiano <batbondik0@gmail.com>
2018-04-29 17:06:13 +0700

    Update configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    title: 'Personal Blog',
+    author: 'Vassiliy Kuzenkov',
+    description: 'My little blog for easy reading.',
+    siteUrl: 'http://bondiano.github.io',
   },
   pathPrefix: '/gatsby-starter-blog',
   plugins: [
